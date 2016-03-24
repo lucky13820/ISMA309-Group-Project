a4ea6cb3df2d096b4883abe4043856414cb6b7ad
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-24 10:18:09 -0700

    remove text in footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Caos
+ */
+
+?>
+        <div class="clearfix"></div>
+<?php if ( !is_single() ) { ?>            
+</div><!-- /#container -->
+<?php } ?>
+
+	<div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <?php get_template_part( '/template-parts/social-menu', 'footer' ); ?>
+                </div>
+
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .sub-footer -->
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
