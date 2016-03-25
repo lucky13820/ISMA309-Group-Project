de296f09bbafa781f3e749792a4724761081fb09
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-25 10:53:10 -0700

    add movie info onto the archive page

--- template-parts/content-box.php
+++ template-parts/content-box.php
@@ -14,14 +14,21 @@
 	<div class="post-box-container">
         <div class="post-box-image" style="background-image: url(<?php the_field('poster'); ?>);"></div>
         <div class="post-box-text">
+            <?php the_title( sprintf( '<h2 class="post-box-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+            <?php the_excerpt(); ?>
+            <span><?php the_field('duration') ?></span>
             <?php
             $categories_list = get_the_category_list( esc_html__( ', ', 'caos' ) );
 			if ( $categories_list ) {
 				printf( '<span class="post-box-meta">%1$s</span>', $categories_list ); // WPCS: XSS OK.
 			}
             ?>
-            <?php the_title( sprintf( '<h2 class="post-box-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-            <?php the_excerpt(); ?>
+            <p><?php the_field('artist_name') ?></p>
+            <p><?php the_field('tagline') ?></p>
+            <div class="trailer_movie">
+                <p>Trailer</p>
+                <p>Full Movie</p>
+            </div>
         </div><!-- .post-box-text -->
 
         <div class="post-complete"></div><!-- .post-complete -->
