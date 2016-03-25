005a64900f4f180cc67e51045427f7425ccc2974
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-24 19:20:45 -0700

    remove google font and custom typeface

--- functions.php
+++ functions.php
@@ -4,10 +4,3 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-
-function load_my_fonts() { 
-	wp_register_style('googleFonts', 'https://fonts.googleapis.com/css?family=Lato:900|Source+Sans+Pro'); 	
-	wp_enqueue_style( 'googleFonts');
- } 
- add_action('wp_print_styles', 'load_my_fonts');
-?>
