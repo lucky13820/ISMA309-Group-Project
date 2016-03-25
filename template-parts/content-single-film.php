be59a73155e93c257c6a7294461fb1267141da88
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-24 17:17:11 -0700

    basic film post layout

--- template-parts/content-single-film.php
+++ template-parts/content-single-film.php
@@ -21,21 +21,24 @@
 		
 
     <div class="post-content">
-
-    	<video class="trailer" autoplay="autoplay">
-            <?php the_field('trailer');?>
-        </video>
-        <div class="video-content">
-            <?php the_title( '<h1 class="post-title">', '</h1>' ); ?>
-            <p class="tagline"><?php the_field('tagline');?></p>
-            <p class="artist-statement"><?php the_field('artist_statement');?></p>
-            <p class="project-description"><?php the_field('project_description_synopsis');?></p>
-        </div>
-        <div class="artist-content">
-            <div class="artist-photo">
-                <img src="<?php the_field('artist_photo',12);?>" alt="">
+    	<div class="post-content-video">
+    	    <video class="trailer" autoplay="autoplay">
+                <?php the_field('trailer');?>
+            </video>
+    	</div>
+        <div class="post-content-info">
+            <div class="video-content">
+                <?php the_title( '<h1 class="post-title">', '</h1>' ); ?>
+                <p class="tagline"><?php the_field('tagline');?></p>
+                <p class="artist-statement"><?php the_field('artist_statement');?></p>
+                <p class="project-description"><?php the_field('project_description_synopsis');?></p>
             </div>
-            <p class="artist_name"><?php the_field('artist_name');?></p>
+            <div class="artist-content">
+                <div class="artist-photo">
+                    <img src="<?php the_field('artist_photo',12);?>" alt="">
+                </div>
+                <p class="artist_name"><?php the_field('artist_name');?></p>
+            </div>
         </div>
     	<div class="entry">
 			<?php the_content(); ?>
