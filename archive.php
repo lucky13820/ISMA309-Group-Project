444695b5643803fdf7a36e41cd7f373579af6705
KevinGao2201 <kevingao.2201@gmail.com>
2016-03-25 01:15:49 -0700

    one group every 4 posts on archive pages

--- archive.php
+++ archive.php
@@ -10,38 +10,48 @@
 get_header(); ?>
 
 	<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-
-			<div class="post-box-list">
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
+        
+        <?php if ( have_posts() ) : ?>
+			<header class="page-header">
 				<?php
-
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content-box', get_post_format() );
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
+			</header><!-- .page-header -->
+        
+            <div class="post-box-list">
 
-			<?php endwhile; ?>
-			
-			</div><!-- .post-box-list -->
+                <?php if (have_posts()) :
+                    $i=0; // counter
+                    while(have_posts()) : the_post();
+                        if($i%4==0) { // if counter is multiple of 4, put an opening div ?>
+                        <!-- <?php echo ($i+1).'-'; echo ($i+4); ?> -->
+                        <div class="row">
+                        <?php } ?>
+                        <?php
+                            /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'template-parts/content-box', get_post_format() );
+                        ?>
+                        <?php $i++;
+                        if($i%4==0) { // if counter is multiple of 4, put an closing div ?>
+                        </div>
+                        <?php } ?>
+                    <?php endwhile; ?>
+                        <?php
+                        if($i%4!=0) { // put closing div here if loop is not exactly a multiple of 4 ?>
+                        </div>
+                        <?php } ?>
+                <?php endif; ?>
 
-			<?php get_template_part( 'template-parts/pagination', 'archive' ); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-
+            </div><!-- .post-box-list -->
+            <?php get_template_part( 'template-parts/pagination', 'archive' ); ?>
+        <?php else : ?>
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php endif; ?>
 
 	</main><!-- #main -->
 
