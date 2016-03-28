f786b723dbca453994a8873c99bbfe0980e4d0a1
Qing Yao <lucky13820@gmail.com>
2016-03-28 15:18:38 -0700

    Get rid of some useless code

--- functions.php
+++ functions.php
@@ -4,37 +4,6 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-/**
- * Load custom post type archive on home page
- *
- * Reference: http://www.wpaustralia.org/wordpress-forums/topic/pre_get_posts-and-is_front_page/
- * Reference: http://wordpress.stackexchange.com/questions/30851/how-to-use-a-custom-post-type-archive-as-front-page
- */
-function prefix_downloads_front_page( $query ) {
-    // Only filter the main query on the front-end
-    if ( is_admin() || ! $query->is_main_query() ) {
-    	return;
-    }
-    global $wp;
-    $front = false;
-	// If the latest posts are showing on the home page
-    if ( ( is_home() && empty( $wp->query_string ) ) ) {
-    	$front = true;
-    }
-	// If a static page is set as the home page
-    if ( ( $query->get( 'page_id' ) == get_option( 'page_on_front' ) && get_option( 'page_on_front' ) ) || empty( $wp->query_string ) ) {
-    	$front = true;
-    }
-    if ( $front ) :
-        $query->set( 'post_type', 'animation' );
-        $query->set( 'page_id', '' );
-        // Set properties to match an archive
-        $query->is_page = 0;
-        $query->is_singular = 0;
-        $query->is_post_type_archive = 1;
-        $query->is_archive = 1;
-    endif;
-}
-add_action( 'pre_get_posts', 'prefix_downloads_front_page' );
+
 
 ?>
--- template-parts/content-box.php
+++ template-parts/content-box.php
@@ -9,13 +9,12 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('post-box'); ?> data-href="<?php the_permalink(); ?>">
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-box'); ?> >
 
 	<div class="post-box-container">
         <div class="post-box-image" style="background-image: url(<?php the_field('poster'); ?>);"></div>
         <div class="post-box-text">
-            <?php the_title( sprintf( '<h2 class="post-box-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-            <?php the_excerpt(); ?>
+            <div class="post-title"><a rel="bookmark" href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></div>
             <span><?php the_field('duration') ?></span>
             <?php
             $categories_list = get_the_category_list( esc_html__( ', ', 'caos' ) );
