d2001eca990cdae1da6b401f0de3fb70a0fd0ca9
Qing Yao <lucky13820@gmail.com>
2016-03-28 15:16:14 -0700

    home page that shows all the posts

---
+++ page-2.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Caos
+ */
+
+get_header(); ?>
+
+
+	<main id="main" class="site-main" role="main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <div class="post-box-list">
+
+							<?php
+
+    // set up our archive arguments
+    $archive_args = array(
+      post_type => array( 'animation', 'film', 'installation' ),     // get only posts
+        // this will display all posts on one page
+    );
+
+    // new instance of WP_Quert
+    $archive_query = new WP_Query( $archive_args );
+
+  ?>
+                <?php if (have_posts()) :
+                    $i=0; // counter
+                    while ( $archive_query->have_posts() ) : $archive_query->the_post();
+                        if($i%4==0) { // if counter is multiple of 4, put an opening div ?>
+                        <!-- <?php echo ($i+1).'-'; echo ($i+4); ?> -->
+                        <div class="row">
+                        <?php } ?>
+                        <?php
+                            /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'template-parts/content-box-home', get_post_format() );
+                        ?>
+                        <?php $i++;
+                        if($i%4==0) { // if counter is multiple of 4, put an closing div ?>
+                        </div>
+                        <?php } ?>
+                    <?php endwhile; ?>
+                        <?php
+                        if($i%4!=0) { // put closing div here if loop is not exactly a multiple of 4 ?>
+                        </div>
+                        <?php } ?>
+                <?php endif; ?>
+
+            </div><!-- .post-box-list -->
+            <?php get_template_part( 'template-parts/pagination', 'archive' ); ?>
+        <?php else : ?>
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php endif; ?>
+
+	</main><!-- #main -->
+
+<?php get_footer(); ?>
---
+++ template-parts/content-box-home.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Caos
+ */
+
+?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('post-box'); ?>>
+
+	<div class="post-box-container">
+        <div class="post-box-image" style="background-image: url(<?php the_field('poster'); ?>);"></div>
+        <div class="post-box-text">
+            <div class="post-title"><a rel="bookmark" href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></div>
+            <span><?php the_field('duration') ?></span>
+            <?php
+            $categories_list = get_the_category_list( esc_html__( ', ', 'caos' ) );
+			if ( $categories_list ) {
+				printf( '<span class="post-box-meta">%1$s</span>', $categories_list ); // WPCS: XSS OK.
+			}
+            ?>
+            <p><?php the_field('artist_name') ?></p>
+            <p><?php the_field('tagline') ?></p>
+            <div class="trailer_movie">
+                <p>Trailer</p>
+                <p>Full Movie</p>
+            </div>
+        </div><!-- .post-box-text -->
+
+        <div class="post-complete"></div><!-- .post-complete -->
+
+    </div><!-- .post-box-container -->
+
+</article><!-- #post-## -->
