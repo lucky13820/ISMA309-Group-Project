7e505baaf70f4380f3d7891aa0a3b5d69589a685
Quinn McCallum <student@dhcp-10-11-10-15.edu.eciad.ca>
2016-03-29 16:16:14 -0700

    Changed path for logo image for compatibility/best practices.

--- header.php
+++ header.php
@@ -46,9 +46,9 @@ if( is_plugin_active( 'rest-api/plugin.php' ) ){
                     $logo = $logo[0];
                     ?>
                     <?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="ql_logo"><?php if ( !empty( $logo ) ) : echo '<img src="http://localhost:8888/mod/wp-content/uploads/2016/03/Group.png" />'; else: bloginfo( 'name' ); endif; ?></a></h1>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="ql_logo"><?php if ( !empty( $logo ) ) : echo '<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2016/03/Group.png" title="Home" alt="home" />'; else: bloginfo( 'name' ); endif; ?></a></h1>
 					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="ql_logo"><img src="http://localhost:8888/mod/wp-content/uploads/2016/03/Group.png" /></a></p>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="ql_logo"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2016/03/Group.png" /></a></p>
 					<?php endif; ?>
                 </div><!-- /logo_container -->
 
