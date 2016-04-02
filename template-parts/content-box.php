ff147b1093a3811dd0b64f3e110091d3b873e453
Quinn <student@dhcp-10-11-10-113.edu.eciad.ca>
2016-04-01 22:20:33 -0700

    Made lightbox proper size and cleaned unnecessary files from plugin download.

--- template-parts/content-box.php
+++ template-parts/content-box.php
@@ -31,7 +31,7 @@
                     <a href="#" data-featherlight="#trailer-lightbox"><p>Trailer</p></a>
                     <div id="trailer-lightbox">
                     
-                       <video width="320" height="240" controls>
+                       <video width="960" height="540" controls>
                            <source src="<?php echo get_site_url(); ?>/wp-content/uploads/2016/03/SampleVideo_1280x720_1mb-1.mp4" type="video/mp4">
                            Your browser does not support the video tag.
                         </video>
